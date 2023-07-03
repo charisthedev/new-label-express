e0406923fb5c1503940ff60ba0a482f7d3bd4e2f
roscoericch <sikirurazak1@gmail.com>
2023-07-03 09:48:56 -0700

    updated video upload

--- controllers/videoUploadCtrl.js
+++ controllers/videoUploadCtrl.js
@@ -99,7 +99,7 @@ const videoUpload = {
             }
           }
         }
-      );
+      ).end(bufferStream);
       // if (firstChunk && fs.existsSync("./uploads/" + tmpFilename)) {
       //   fs.unlinkSync("./uploads/" + tmpFilename);
       // }
