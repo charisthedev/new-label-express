f7bb222ee1dd80f3d04acd219ef0b02a1bbc60dc
roscoericch <sikirurazak1@gmail.com>
2023-07-04 15:22:16 -0700

    added auth to stream route

--- routes/video-streamRouter.js
+++ routes/video-streamRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
 const videoStreamCtrl = require("../controllers/video-streamingCtrl");
 
-router.get("/:id", videoStreamCtrl.sendVideoUrl);
+router.get("/:id", auth, videoStreamCtrl.sendVideoUrl);
 
 module.exports = router;
