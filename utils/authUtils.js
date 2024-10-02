fd80cc22335bab54f84f939c29c0e829f743b0d8
sikiru razak <sikirurazak1@gmail.com>
2024-10-02 11:59:49 +0100

    update router auth check

--- utils/authUtils.js
+++ utils/authUtils.js
@@ -2,10 +2,12 @@ const jwt = require('jsonwebtoken');
 
 const AuthUtil = async (req) => {
   const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.split(' ').length < 2) {
+    return false;
+  }
   const token = authHeader.split(' ')[1];
   const decodedToken = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-  const { id } = decodedToken.data; // Destructure id from decoded toke
-  console.log(id);
+  const { id } = decodedToken.data; 
   return id;
 }
 
