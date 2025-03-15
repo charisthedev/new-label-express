50f4b6b2e7ccfb1840928ecc195eba657f391513
sikiru razak <sikirurazak1@gmail.com>
2025-03-15 22:46:24 +0100

    fix: ipv4/ipv6 ip address formatting

--- utils/location.js
+++ utils/location.js
@@ -1,12 +1,23 @@
 const axios = require("axios");
 
 const getLocationDetails = async (ip) => {
-    const geo = await axios.get(`http://ip-api.com/json/${ip.split(",")[0]}`)
+  let address = ip.split(",")[0];
+  if (address.startsWith("::ffff:")) {
+    address = address.substring(7);
+  }
+  console.log(ip, address);
+  try {
+    const geo = await axios.get(`http://ip-api.com/json/${address}`);
     const country = geo?.data?.country;
-    const currencyResponse = await axios.get(`https://restcountries.com/v3.1/name/${country}`);
+    const currencyResponse = await axios.get(
+      `https://restcountries.com/v3.1/name/${country}`
+    );
     const currency = currencyResponse.data[0].currencies;
     const currencyCode = Object.keys(currency)[0].toLowerCase();
     return currencyCode;
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = getLocationDetails;
