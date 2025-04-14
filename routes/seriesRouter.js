2d28fc488af6bb65dba2545618b6c9252383a22b
sikiru razak <sikirurazak1@gmail.com>
2025-04-14 17:53:07 +0100

    fix: add currency middleware

--- routes/seriesRouter.js
+++ routes/seriesRouter.js
@@ -1,15 +1,16 @@
 const router = require("express").Router();
 const seriesCtrl = require("../controllers/seriesCtrl");
 const modifiedAuthAdmin = require("../middleware/modifiedAuthAdmin");
+const checkCurrency = require("../middleware/location");
 
 router
   .route("/")
-  .get(seriesCtrl.getAllSeries)
+  .get(checkCurrency, seriesCtrl.getAllSeries)
   .post(modifiedAuthAdmin("Series"), seriesCtrl.createSeries);
 
 router
   .route("/:id")
-  .get(seriesCtrl.getSingleSeries)
+  .get(checkCurrency, seriesCtrl.getSingleSeries)
   .delete(modifiedAuthAdmin("Series"), seriesCtrl.deleteASeries)
   .put(modifiedAuthAdmin("Series"), seriesCtrl.updateASeries);
 
